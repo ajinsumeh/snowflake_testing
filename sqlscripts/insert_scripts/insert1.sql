b71e531ed9881bf3a4a086ed2b52fad9c9e07bde
Ajin Sumesh <76661449+ajinsumeh@users.noreply.github.com>
2024-08-27 18:32:38 +0530

    Update insert1.sql

--- sqlscripts/insert_scripts/insert1.sql
+++ sqlscripts/insert_scripts/insert1.sql
@@ -1,1 +1,1 @@
-insert into v_env_test_db_prod.v_env_PUBLIC_schema.employee values ('BCD','789','Y');
+insert into v_env_test_db_prod.v_env_PUBLIC_schema.v_env_employee_tbl values ('BCD','789','Y');
