10389a4e93fc815f56757bb02f11931ba3d7e7fe
Ajin Sumesh <76661449+ajinsumeh@users.noreply.github.com>
2024-10-18 17:35:46 +0530

    Update test_employee_1.sql

--- sqlscripts/insert_scripts/test_employee_1.sql
+++ sqlscripts/insert_scripts/test_employee_1.sql
@@ -1,4 +1,4 @@
-insert into github_repo.TEST.employee2 values (1,'Ajin','Y');
+insert into github_repo.TEST.employee2 values (3,'Ajin','Y');
 
-insert into github_repo.TEST.employee2 values (2,'Ajin2','Y');
+--insert into github_repo.TEST.employee2 values (2,'Ajin2','Y');
 
