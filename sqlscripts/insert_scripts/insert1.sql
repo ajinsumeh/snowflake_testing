8c60f0b6a316ea689f53fee1ce683f3f1fbf090f
Ajin Sumesh <76661449+ajinsumeh@users.noreply.github.com>
2024-10-24 01:58:29 +0530

    Update insert1.sql

--- sqlscripts/insert_scripts/insert1.sql
+++ sqlscripts/insert_scripts/insert1.sql
@@ -1,4 +1,3 @@
-insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE values (1,'Ajin',000);
+insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE values (1,'Ajin','000');
 --insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE values (1,'Ajin',000);
-
-insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE values (2,'Ajin1',001);
+insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE values (2,'Ajin1','001');
