5e6b4e758e68a53675ad40ae0cd5da968c4299c6
Ajin Sumesh <76661449+ajinsumeh@users.noreply.github.com>
2024-10-25 20:54:00 +0530

    Add files via upload

--- sqlscripts/insert_scripts/snowflake_query.sql
+++ sqlscripts/insert_scripts/snowflake_query.sql
@@ -1,3 +1,5 @@
 insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE values (3,'Ajin3','003');
 insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE1 values (3,'Ajin3','003');
 insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE values (4,'Ajin4','004');
+
+update TEST_DATABASE.GIT_FETCH.EMPLOYEE set name='ABCD' where name like '%Ajin%';
