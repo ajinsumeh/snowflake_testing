e53cbf4340e84a8b21072a345bfad46e4695a072
Ajin Sumesh <76661449+ajinsumeh@users.noreply.github.com>
2024-10-28 19:20:30 +0530

    Add files via upload

--- sqlscripts/insert_scripts/snowflake_query.sql
+++ sqlscripts/insert_scripts/snowflake_query.sql
@@ -1,3 +1,4 @@
-insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE values (4,'Ajin4','004');
+insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE values (3,'Ajin3','003');
+insert into TEST_DATABASE.GIT_FETCH.EMPLOYEE values (6,'Ajin4','004');
 
 
